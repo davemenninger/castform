e71d44262546f0bd3d384f234b020b730e9dd810
Dave Menninger <dave.menninger@gmail.com>
2023-07-20 19:41:54 -0400

    let's add a droplet

--- home.tf
+++ home.tf
@@ -14,7 +14,7 @@ provider "digitalocean" {}
 resource "digitalocean_project" "castform" {
   name        = "castform"
   description = "a smol home in the cloud"
-  resources = [digitalocean_app.home.urn]
+  resources   = [digitalocean_app.home.urn, digitalocean_droplet.charjabug.urn]
 }
 
 resource "digitalocean_app" "home" {
@@ -32,4 +32,41 @@ resource "digitalocean_app" "home" {
       }
     }
   }
+}
+
+resource "digitalocean_vpc" "vermilion" {
+  name     = "vermilion"
+  region   = "nyc1"
+  ip_range = "10.110.0.0/16"
+}
+
+resource "digitalocean_firewall" "ponyta" {
+  name = "ponyta"
+
+  droplet_ids = [digitalocean_droplet.charjabug.id]
+  // no inbound yet
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
+resource "digitalocean_droplet" "charjabug" {
+  name     = "charjabug"
+  size     = "s-1vcpu-1gb"
+  image    = "ubuntu-22-04-x64"
+  region   = "nyc1"
+  vpc_uuid = digitalocean_vpc.vermilion.id
 }
