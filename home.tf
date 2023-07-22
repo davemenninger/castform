5ededd9ab3bd9bdb96dc0864ebe39e0aea08a0b2
Dave Menninger <dave.menninger@gmail.com>
2023-07-21 21:57:59 -0400

    it's not DNS... it's DNS

--- home.tf
+++ home.tf
@@ -14,10 +14,11 @@ provider "digitalocean" {}
 resource "digitalocean_project" "castform" {
   name        = "castform"
   description = "a smol home in the cloud"
-  resources   = [
-    digitalocean_app.home.urn, 
+  resources = [
+    digitalocean_app.home.urn,
     digitalocean_droplet.charjabug.urn,
-    digitalocean_volume.loudred.urn
+    digitalocean_volume.loudred.urn,
+    digitalocean_domain.electroweb.urn
   ]
 }
 
@@ -83,6 +84,8 @@ resource "digitalocean_firewall" "ponyta" {
     protocol              = "icmp"
     destination_addresses = ["0.0.0.0/0", "::/0"]
   }
+
+  # tags = ["fire_type"]
 }
 
 resource "digitalocean_droplet" "charjabug" {
@@ -92,7 +95,7 @@ resource "digitalocean_droplet" "charjabug" {
   region    = "nyc1"
   vpc_uuid  = digitalocean_vpc.vermilion.id
   user_data = file("charjabug.yaml")
-  tags = ["electric_type", "bug_type"]
+  tags      = ["electric_type", "bug_type"]
 }
 
 output "herpderp" {
@@ -100,15 +103,40 @@ output "herpderp" {
 }
 
 resource "digitalocean_volume" "loudred" {
-  region = "nyc1"
-  name = "loudred"
-  size = 2
-  initial_filesystem_type = "ext4"
+  region                   = "nyc1"
+  name                     = "loudred"
+  size                     = 2
+  initial_filesystem_type  = "ext4"
   initial_filesystem_label = "loudred"
-  tags = ["normal_type"]
+  tags                     = ["normal_type"]
 }
 
 resource "digitalocean_volume_attachment" "roar" {
   droplet_id = digitalocean_droplet.charjabug.id
   volume_id  = digitalocean_volume.loudred.id
+}
+
+resource "digitalocean_domain" "electroweb" {
+  name       = "djm.quest"
+  ip_address = digitalocean_droplet.charjabug.ipv4_address
+}
+
+# https://github.com/hashicorp/terraform/issues/23893
+module "test" {
+  source = "matti/urlparse/external"
+  url    = digitalocean_app.home.live_url
+}
+
+resource "digitalocean_record" "www" {
+  domain = digitalocean_domain.electroweb.id
+  type   = "CNAME"
+  name   = "www"
+  value  = "${module.test.host}."
+}
+
+resource "digitalocean_record" "charjabug" {
+  domain = digitalocean_domain.electroweb.id
+  type   = "A"
+  name   = "charjabug"
+  value  = digitalocean_droplet.charjabug.ipv4_address
 }
